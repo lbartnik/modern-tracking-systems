356d565e7ecab7e41f500241c29ff3c02403d8a7
Lukasz A. Bartnik <lbartnik@protonmail.com>
2025-05-29 22:02:34 -0700

    Refactor base target

--- tracking_v2/target/target.py
+++ tracking_v2/target/target.py
@@ -10,8 +10,37 @@ class Target(object):
     name: str
     spatial_dim: int
     seed: int
+    rng: np.random.Generator
+    T: float
+    n: int
+    cached: np.ndarray
 
-    def true_states(self, T: Union[float, ArrayLike] = 1, n: int = 400, seed: int = None) -> np.ndarray:
+    def cache(self, T: float = 1, n: int = 400):
+        self.T = T
+        self.n = n
+        self.cached = self.true_states(T, n)
+
+    def reset_seed(self, seed: int = 0):
+        self.seed = seed
+        self.rng = np.random.default_rng(seed=seed)
+
+    def reset_rng(self, rng: np.random.Generator = None):
+        self.seed = None
+        self.rng = rng
+
+    def __repr__(self):
+        if self.seed is None:
+            return self.name
+        else:
+            return f"{self.name}_{self.seed}"
+
+    def true_state(self, t: float) -> np.ndarray:
+        if self.cached is not None:
+            return self.cached[t, :]
+        else:
+            return self.true_states()[t, :]
+
+    def true_states(self, T: float = 1, n: int = 400) -> np.ndarray:
         """Generate target states.
 
         Args:
@@ -26,15 +55,3 @@ class Target(object):
             np.ndarray: (n, 6) array of states.
         """
         raise Exception(f"Target {self.__class__.__name__} does not implement true_states()")
-
-    def true_state(self, t: float) -> np.ndarray:
-        return self.true_states()[t, :]
-
-    def reset_seed(self, seed: int = 0):
-        raise Exception(f"Target {self.__class__.__name__} does not implement reset_seed()")
-    
-    def reset_rng(self, rng: np.random.Generator = None):
-        raise Exception(f"Target {self.__class__.__name__} does not implement reset_rng()")
-
-    def __repr__(self):
-        return self.name
